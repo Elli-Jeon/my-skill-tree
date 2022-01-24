36929fb9f21220d3ffa3bf052fc0c4e87934ae0d
raverena <raverena96@gmail.com>
2022-01-24 19:30:51 +0900

    [Add] home page

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,16 +1,44 @@
 // __tests__/index.test.jsx
 
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Home from "pages/index";
 
 describe("Home", () => {
-	it("renders a heading", () => {
+	it("제목", () => {
 		render(<Home />);
 
-		const heading = screen.getByRole("heading", {
-			name: /welcome to next\.js!/i,
-		});
+		const heading = screen.getByText("My Tech Tree");
 
 		expect(heading).toBeInTheDocument();
+	});
+
+	it("만든 사람", () => {
+		render(<Home />);
+
+		const madeBy = screen.getByText("Elli-Jeon");
+
+		expect(madeBy).toBeInTheDocument();
+	});
+
+	it("version", () => {
+		render(<Home />);
+
+		const version = screen.getByText("ver1.1");
+
+		expect(version).toBeInTheDocument();
+	});
+
+	it("이미지", () => {
+		render(<Home />);
+
+		const github = screen.getByAltText("github");
+		const email = screen.getByAltText("email");
+		const kakao = screen.getByAltText("kakao");
+		const google = screen.getByAltText("google");
+
+		[github, email, kakao, google].forEach((ele) => {
+			expect(ele).toBeInTheDocument();
+		});
 	});
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-	return <div></div>;
+	return (
+		<div>
+			<span>My Tech Tree</span>
+			<span>Elli-Jeon</span>
+			<span>ver1.1</span>
+			<Image src={"/logo/github.svg"} alt="github" width="50px" height="50px" />
+			<Image src={"/logo/email.svg"} alt="email" width="50px" height="50px" />
+			<Image src={"/logo/kakao.svg"} alt="kakao" width="40px" height="100px" />
+			<Image
+				src={"/logo/google.svg"}
+				alt="google"
+				width="40px"
+				height="100px"
+			/>
+		</div>
+	);
 };
 
 export default Home;
