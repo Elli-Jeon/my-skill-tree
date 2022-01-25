5253ca81fef2d000feefe466a7d7046f31ce7ce6
Elli-Jeon <raverena96@gmail.com>
2022-01-25 20:41:38 +0900

    [Update] github, email link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import GithubIcon from "public/logo/github.svg";
 import Email from "public/logo/email.svg";
 import LoginButton from "components/LoginButton";
@@ -16,8 +17,16 @@ const Home: NextPage = () => {
 					<span>Elli-Jeon</span>
 					<span>ver1.0</span>
 					<div className="contact">
-						<GithubIcon fill="white" width="4vh" />
-						<Email fill="white" width="4vh" />
+						<Link href="https://github.com/Elli-Jeon">
+							<a target="_blank">
+								<GithubIcon fill="white" width="4vh" />
+							</a>
+						</Link>
+						<Link href="mailto:raverena96@gmail.com">
+							<a>
+								<Email fill="white" width="4vh" />
+							</a>
+						</Link>
 					</div>
 				</div>
 				<div className="login">
