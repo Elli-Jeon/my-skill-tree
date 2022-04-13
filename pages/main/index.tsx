92ca81221e4a5800a918f9a95637fecef33a0a6a
raverena <raverena96@gmail.com>
2022-04-13 17:53:38 +0900

    [Add] fabric

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fabric } from "fabric";
 import NodeMenu from "components/NodeMenu";
 import * as S from "./styles";
 
 const Main = () => {
+	const [canvas, setCanvas] = useState();
+
+	const initCanvas = () => {
+		new fabric.Canvas("canvas", {
+			width: 800,
+			height: 800,
+			backgroundColor: "pink",
+		});
+	};
+
+	useEffect(() => {
+		// setCanvas(initCanvas())
+	}, []);
+
 	return (
 		<S.Container>
+			<canvas id="canvas"></canvas>
 			<S.PositionContainer left={25} bottom={30}>
 				<NodeMenu />
 			</S.PositionContainer>
