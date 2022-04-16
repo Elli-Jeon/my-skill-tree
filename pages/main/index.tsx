6d6e4624237257a7b22b8812c548defd6c265a10
raverena <raverena96@gmail.com>
2022-04-16 18:40:08 +0900

    [Add] rect 그려보기

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
+import type { Canvas } from "fabric/fabric-impl";
 import { fabric } from "fabric";
 import NodeMenu from "components/NodeMenu";
 import * as S from "./styles";
 
 const Main = () => {
-	const [canvas, setCanvas] = useState();
+	const [canvas, setCanvas] = useState<null | Canvas>(null);
 
 	const initCanvas = () => {
-		new fabric.Canvas("canvas", {
+		return new fabric.Canvas("canvas", {
 			width: 800,
 			height: 800,
 			backgroundColor: "pink",
@@ -15,11 +16,23 @@ const Main = () => {
 	};
 
 	useEffect(() => {
-		// setCanvas(initCanvas())
+		setCanvas(initCanvas());
 	}, []);
+
+	const addRect = (canv: null | Canvas) => {
+		const rect = new fabric.Rect({
+			height: 200,
+			width: 200,
+			fill: "yellow",
+		});
+
+		canv?.add(rect);
+		canv?.renderAll();
+	};
 
 	return (
 		<S.Container>
+			<button onClick={() => addRect(canvas)}>rect</button>
 			<canvas id="canvas"></canvas>
 			<S.PositionContainer left={25} bottom={30}>
 				<NodeMenu />
